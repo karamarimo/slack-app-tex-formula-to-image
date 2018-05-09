2363a3322855f927ca23a505d112c49400756a39
karamarimo <kubotaryou.ryoukubota@gmail.com>
2018-05-09 20:33:14 +0900

    fix

--- server.js
+++ server.js
@@ -112,11 +112,19 @@ async function texToPngBuffer(tex) {
 }
 
 function svgToPngBuffer(svg) {
+  return gmToBuffer(gm(Buffer.from(svg)).setFormat("png"))
+}
+
+function gmToBuffer (data) {
   return new Promise((resolve, reject) => {
-    gm(Buffer.from(svg))
-    .toBuffer('PNG', function (err, buffer) {
-      if (err) return reject(err);
-      resolve(buffer)
+    data.stream((err, stdout, stderr) => {
+      if (err) { return reject(err) }
+      const chunks = []
+      stdout.on('data', (chunk) => { chunks.push(chunk) })
+      // these are 'once' because they can and do fire multiple times for multiple errors,
+      // but this is a promise so you'll have to deal with them one at a time
+      stdout.once('end', () => resolve(Buffer.concat(chunks)))
+      stderr.once('data', (data) => reject(String(data)))
     })
   })
 }
